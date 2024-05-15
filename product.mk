02a77be7039406c8317a911ee99090a136785c50
elpaablo <donopwhile1@gmail.com>
2024-05-15 10:14:14 +0000

    Match AlphaDroid launcher options logic

--- product.mk
+++ product.mk
@@ -1,4 +1,10 @@
-PRODUCT_PACKAGES += \
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+ifeq ($(TARGET_LAUNCHER),2)
+  PRODUCT_PACKAGES += \
     Lawnchair \
     LawnchairOverlay \
     Lawnicons
+endif
