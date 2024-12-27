74777c5fc10db92407f1a46a640e58a4cf8b7d81
kshitij-vk <koyande72@gmail.com>
2024-12-27 13:43:09 +0530

    extra: Nuke Lawnchair for a15

--- product.mk
+++ product.mk
@@ -1,23 +1,3 @@
-# Lawnchair
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER ?= true
-ifeq ($(strip $(TARGET_PREBUILT_LAWNCHAIR_LAUNCHER)),true)
-PRODUCT_PACKAGES += \
-    Lawnchair \
-    LawnchairOverlay \
-    Lawnicons
-
-# Lawnchair Launcher
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.quickswitch_lawnchair_shipped=1
-else
-# Lawnchair Launcher
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.quickswitch_lawnchair_shipped=0
-endif
-
-PRODUCT_PACKAGES += \
-    SearchHelper
-
 TARGET_PREBUILT_BCR ?= true
 
 # Basic call recorder
